e076981f0560dbb0ec80a1b23ee51152d2140d5b
CamilaLopez <camilallopez95@gmail.com>
2023-06-22 21:10:20 +0200

    update imports

--- app/src/main/java/com/example/entregaandroidavanzadov2/ui/LoginActivity.kt
+++ app/src/main/java/com/example/entregaandroidavanzadov2/ui/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.entregaandroidavanzadov2.ui
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
--- app/src/test/java/com/example/entregaandroidavanzadov2/data/RepositoryLoginImplTest.kt
+++ app/src/test/java/com/example/entregaandroidavanzadov2/data/RepositoryLoginImplTest.kt
@@ -2,17 +2,11 @@ package com.example.entregaandroidavanzadov2.data
 
 import com.example.entregaandroidavanzadov2.data.remote.RemoteDataSource
 import com.example.entregaandroidavanzadov2.data.remote.fakes.FakeRemoteDataSource
-import com.example.entregaandroidavanzadov2.utils.generateLoginToken
 import com.google.common.truth.Truth
-import io.mockk.coEvery
-import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import retrofit2.Response
-
 class RepositoryLoginImplTest{
 
     private lateinit var repositoryLoginImpl: RepositoryLoginImpl
